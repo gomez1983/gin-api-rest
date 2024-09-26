360679b3caf5e83404103cb672e655ada1a8283d
Andre Gomez <54462949+gomez1983@users.noreply.github.com>
2024-09-26 08:46:34 -0300

    Acrescentado anotações dos códigos

--- main.go
+++ main.go
@@ -2,12 +2,13 @@ package main
 
 import "github.com/gin-gonic/gin"
 
-func ExibeTodosAlunos(c *gin.Context) {
-	c.JSON(200, gin.H{
-		"id":   "1",
-		"nome": "André Gomez",
+func ExibeTodosAlunos(c *gin.Context) { /** Define a função que recebe o contexto da requisição **/
+	c.JSON(200, gin.H{ /** Responde com um status HTTP 200 e um JSON contendo um mapa de chave-valor **/
+		"id":   "1",           /** Chave "id" com valor "1" **/
+		"nome": "André Gomez", /** Chave "nome" com valor "André Gomez" **/
 	})
 }
+
 func main() { /** Função principal que inicia o servidor **/
 	r := gin.Default() /** Cria uma nova instância do roteador Gin com middleware padrão (logger e recovery) **/
 
