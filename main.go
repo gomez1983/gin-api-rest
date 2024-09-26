faa697dc47f5e465791289e6ced9fbb5bfc4fa36
Andre Gomez <54462949+gomez1983@users.noreply.github.com>
2024-09-26 08:45:41 -0300

    Primeira rota com GIN

--- main.go
+++ main.go
@@ -2,7 +2,15 @@ package main
 
 import "github.com/gin-gonic/gin"
 
+func ExibeTodosAlunos(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"id":   "1",
+		"nome": "André Gomez",
+	})
+}
 func main() { /** Função principal que inicia o servidor **/
 	r := gin.Default() /** Cria uma nova instância do roteador Gin com middleware padrão (logger e recovery) **/
-	r.Run(":5000")     /** Inicia o servidor na porta 5000 **/
+
+	r.GET("/alunos", ExibeTodosAlunos)
+	r.Run(":5000") /** Inicia o servidor na porta 5000 **/
 }
