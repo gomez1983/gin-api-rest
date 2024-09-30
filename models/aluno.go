16d0f81e2ef202689c3f3ec85270b6fbac6f1eae
Andre Gomez <54462949+gomez1983@users.noreply.github.com>
2024-09-30 09:05:00 -0300

    Criando validações com o validatorV2

--- models/aluno.go
+++ models/aluno.go
@@ -1,10 +1,20 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"gopkg.in/validator.v2"
+	"gorm.io/gorm"
+)
 
-type Aluno struct { /** Define a estrutura "Aluno" com três campos **/
+type Aluno struct { /** Define a estrutura "Aluno" com quatro campos **/
 	gorm.Model        /** `gorm.Model` adiciona automaticamente os campos ID, CreatedAt, UpdatedAt e DeletedAt aos modelos. **/
-	Nome       string `json:"nome"` /** Campo "Nome" do tipo string, que será convertido para "nome" no JSON **/
-	CPF        string `json:"cpf"`  /** Campo "CPF" do tipo string, que será convertido para "cpf" no JSON **/
-	RG         string `json:"rg"`   /** Campo "RG" do tipo string, que será convertido para "rg" no JSON **/
+	Nome       string `json:"nome" validate:"nonzero"` /** Campo "Nome" do tipo string, que será convertido para "nome" no JSON e não pode ser vazio **/
+	CPF        string `json:"cpf" validate:"len=9"`    /** Campo "CPF" do tipo string, que será convertido para "cpf" no JSON e deve ter exatamente 9 caracteres **/
+	RG         string `json:"rg" validate:"len=11"`    /** Campo "RG" do tipo string, que será convertido para "rg" no JSON e deve ter exatamente 11 caracteres **/
+}
+
+func ValidaDadosDeAluno(aluno *Aluno) error { /** Função que valida os dados de um aluno usando um validador **/
+	if err := validator.Validate(aluno); err != nil { /** Verifica se há erros de validação nos dados do aluno **/
+		return err /** Retorna o erro, se houver **/
+	}
+	return nil /** Retorna nil se não houver erros, indicando que os dados são válidos **/
 }
