3c96ce8b35ce7336df0434a5d97dda98f724e9a4
Andre Gomez <54462949+gomez1983@users.noreply.github.com>
2024-10-16 08:55:15 -0300

    Teste de StatusCode adicionado

--- main_test.go
+++ main_test.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/gin-gonic/gin"
+	"github.com/gomez1983/api-go-gin/controllers"
 )
 
 func SetupDasRotasDeTeste() *gin.Engine { /** Função que configura as rotas para testes **/
@@ -11,6 +14,13 @@ func SetupDasRotasDeTeste() *gin.Engine { /** Função que configura as rotas pa
 	return rotas           /** Retorna o roteador configurado **/
 }
 
-func TestFalhador(t *testing.T) { /** Função de teste que falha propositalmente **/
-	t.Fatalf("Teste falhou de propósito. Não se preocupe") /** Força uma falha no teste e exibe a mensagem especificada **/
+func TestVerificaStatusCodeDaSaudacaoComParametro(t *testing.T) { /** Função de teste que verifica o status code da rota de saudação **/
+	r := SetupDasRotasDeTeste()                     /** Configura as rotas de teste utilizando o Gin **/
+	r.GET("/:nome", controllers.Saudacao)           /** Define a rota GET que recebe um parâmetro "nome" e chama a função Saudacao no controlador **/
+	req, _ := http.NewRequest("GET", "/andre", nil) /** Cria uma nova requisição HTTP GET com o parâmetro "andre" na URL **/
+	resposta := httptest.NewRecorder()              /** Cria um gravador de resposta para simular o comportamento do servidor **/
+	r.ServeHTTP(resposta, req)                      /** Envia a requisição e obtém a resposta simulada **/
+	if resposta.Code != http.StatusOK {             /** Verifica se o código de status da resposta é diferente de 200 OK **/
+		t.Fatalf("Status error: valor recebido foi %d e o esperado foi %d", resposta.Code, http.StatusOK) /** Falha no teste se o status code não for 200 OK, mostrando a diferença **/
+	}
 }
