5e0244b269b81acdc74c67862d999e7ff03b1362
Andre Gomez <54462949+gomez1983@users.noreply.github.com>
2024-10-22 09:22:08 -0300

    Adicionado pacote Assert para testes

--- main_test.go
+++ main_test.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"fmt"
+	"io"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
 	"github.com/gin-gonic/gin"
 	"github.com/gomez1983/api-go-gin/controllers"
+	"github.com/stretchr/testify/assert"
 )
 
 func SetupDasRotasDeTeste() *gin.Engine { /** Função que configura as rotas para testes **/
@@ -15,12 +18,15 @@ func SetupDasRotasDeTeste() *gin.Engine { /** Função que configura as rotas pa
 }
 
 func TestVerificaStatusCodeDaSaudacaoComParametro(t *testing.T) { /** Função de teste que verifica o status code da rota de saudação **/
-	r := SetupDasRotasDeTeste()                     /** Configura as rotas de teste utilizando o Gin **/
-	r.GET("/:nome", controllers.Saudacao)           /** Define a rota GET que recebe um parâmetro "nome" e chama a função Saudacao no controlador **/
-	req, _ := http.NewRequest("GET", "/andre", nil) /** Cria uma nova requisição HTTP GET com o parâmetro "andre" na URL **/
-	resposta := httptest.NewRecorder()              /** Cria um gravador de resposta para simular o comportamento do servidor **/
-	r.ServeHTTP(resposta, req)                      /** Envia a requisição e obtém a resposta simulada **/
-	if resposta.Code != http.StatusOK {             /** Verifica se o código de status da resposta é diferente de 200 OK **/
-		t.Fatalf("Status error: valor recebido foi %d e o esperado foi %d", resposta.Code, http.StatusOK) /** Falha no teste se o status code não for 200 OK, mostrando a diferença **/
-	}
+	r := SetupDasRotasDeTeste()                                          /** Configura as rotas de teste utilizando o Gin **/
+	r.GET("/:nome", controllers.Saudacao)                                /** Define a rota GET que recebe um parâmetro "nome" e chama a função Saudacao no controlador **/
+	req, _ := http.NewRequest("GET", "/andre", nil)                      /** Cria uma nova requisição HTTP GET com o parâmetro "andre" na URL **/
+	resposta := httptest.NewRecorder()                                   /** Cria um gravador de resposta para simular o comportamento do servidor **/
+	r.ServeHTTP(resposta, req)                                           /** Envia a requisição e obtém a resposta simulada **/
+	assert.Equal(t, http.StatusOK, resposta.Code, "Deveriam ser iguais") /** Verifica se o código de status é 200 OK e exibe a mensagem personalizada se falhar **/
+	mockDaResposta := `{"API diz":"E aí andre, tudo beleza?"}`
+	respostaBody, _ := io.ReadAll(resposta.Body)
+	assert.Equal(t, mockDaResposta, string(respostaBody))
+	fmt.Println(string(respostaBody))
+	fmt.Println(mockDaResposta)
 }
